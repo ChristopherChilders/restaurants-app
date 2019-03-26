cb1fa26afae0a8044261d438e37ddffb4a036e03
Christopher Childers <christopher.childers13@yahoo.com>
2019-03-26 15:19:33 -0400

    adds a constructor to restaurants

--- models/restaurants.js
+++ models/restaurants.js
@@ -3,12 +3,24 @@ const db = require('./conn');
 
 //declare the class
 class Restaurant {
+    //need a constuctor to id the values i'll be searching for
+    constructor(id, name, address, street, state, phone, menu, picture){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.street = street;
+        this.state = state;
+        this.phone = phone;
+        this.menu = menu;
+        this.picture = picture;
+    }
     //getAll is a static method
     static getAll() {
         // .any returns 0 or more results in an array
         // but that's async, so we return the call to db.any
         return db.any('select * from restaurants');
     }
+    
 }
 
 // export the class

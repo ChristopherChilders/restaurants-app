5e1bf2a68a2dd54631e3f615e7b71e886f12fec3
Christopher Childers <christopher.childers13@yahoo.com>
2019-03-28 11:52:16 -0400

    adds a serve function to the app

---
+++ index.js
@@ -0,0 +1,51 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+//Import my model class
+const Reviews = require('./models/reviews')
+const Users = require('./models/user')
+
+const server = http.createServer(async (req, res) => {
+    console.log(req.url);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+
+    //if req.url is "/users", send them all users
+    //else if it doesnt match either, send a welcome message
+
+    if (req.url === "/reviews"){
+        const allReviews = await Reviews.getAll();
+        const reviewJSON = JSON.stringify(allReviews);
+        res.end(reviewJSON);
+    } else if (req.url.startsWith("/users")){
+        const parts = req.url.split("/")
+        console.log(parts);
+        //when the req.url is "/users", parts is ['', 'users']
+        //when the req.url is "/users/3", parts is ['', 'users', '3']
+        if(parts.length === 2){
+            const allUsers = await Users.getAll();
+            const userJson = JSON.stringify(allUsers);
+            res.end(userJson);
+        } else if (parts.length === 3){
+            const userId = parts[2];
+            const theUser = await Users.getById(userId);
+            const userJson = JSON.stringify(theUser);
+            res.end(userJson)
+        } else {
+            res.statusCode = 404;
+            res.end('Resource not found.')
+        }
+
+    } else {
+        res.end(`{message: "thank you for your patronage. please send bitcoin."}`);
+    } 
+        
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}`);
+});
--- models/user.js
+++ models/user.js
@@ -37,6 +37,17 @@ class User {
                     })
     }
 
+    static getAll(){
+        return db.any(`select * from users`)
+        .then((arrayOfUsers) => {
+            return arrayOfUsers.map((userData) => {
+                const aUser = new User(userData.id, userData.first_name, userData.last_name, 
+                    userData.email, userData.password)
+                return aUser
+            })
+        });
+    }
+
     //no static since this is an instance method
     //it belongs to the individual instance
     save(){
@@ -98,7 +109,9 @@ class User {
                             data.user_id,
                             data.restaurants_id
                         )
+                        arrayOfFavoritesData.push(newInstance);
                     })
+                    return arrayOfFavoritesInstances;
                 });
     }
 };
